ace63556e1c2cb4b53500aa3eee3b92cae0044d4
cjprsnin <46409875+cjprsnin@users.noreply.github.com>
2025-02-06 14:11:16 -0600

    Update MMM-DogeClock.js

--- MMM-DogeClock.js
+++ MMM-DogeClock.js
@@ -4,12 +4,12 @@ Module.register("MMM-DogeClock", {
     width: "100%",
     height: "300px"
   },
-  getDom: function() {
-    const iframe = document.createElement("iframe");
-    iframe.src = this.config.url;
-    iframe.style.width = this.config.width;
-    iframe.style.height = this.config.height;
-    iframe.style.border = "none";
-    return iframe;
-  }
-});
+getDom: function() {
+  const iframe = document.createElement("iframe");
+  iframe.src = this.config.url;
+  iframe.style.width = this.config.width;
+  iframe.style.height = this.config.height;
+  iframe.style.border = "none";
+  iframe.setAttribute("scrolling", "no"); // Disable scrollbars
+  return iframe;
+}
