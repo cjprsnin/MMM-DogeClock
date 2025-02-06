d58f1dd4f9ec7e4b35a964487fcc9a1c9804b2ef
cjprsnin <46409875+cjprsnin@users.noreply.github.com>
2025-02-06 14:04:26 -0600

    Create MMM-DogeClock.js

---
+++ MMM-DogeClock.js
@@ -0,0 +1,15 @@
+Module.register("MMM-DogeClock", {
+  defaults: {
+    url: "https://dogegov.com/dogeclock.html",
+    width: "100%",
+    height: "300px"
+  },
+  getDom: function() {
+    const iframe = document.createElement("iframe");
+    iframe.src = this.config.url;
+    iframe.style.width = this.config.width;
+    iframe.style.height = this.config.height;
+    iframe.style.border = "none";
+    return iframe;
+  }
+});
